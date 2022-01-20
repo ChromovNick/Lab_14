b5074d0aa075e3a0876821bd3eb12b5130df5ebe
Никита Хромов <chromovnick@gmail.com>
2022-01-20 22:17:08 +0300

    Добавьте файлы проекта.

---
+++ Lab_14/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog doggo = new Dog("Пёс");
+            doggo.ShowInfo();
+            Cat citty = new Cat("Кошка");
+            citty.ShowInfo();
+            Console.ReadKey();
+        }
+    }
+}
